ecac3914cc70e93702543a3a666696939dbf3d82
mayura-andrew <mayuraalahakoon@gmail.com>
2023-09-10 14:27:14 +0530

    impleted services

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,6 +8,8 @@ use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
     /**
@@ -15,10 +17,17 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request  $request)
     {
         //
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+       // return new CustomerCollection(Customer::paginate());
+       if(count($queryItems) == 0){
         return new CustomerCollection(Customer::paginate());
+        }else{
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
     }
 
     /**
